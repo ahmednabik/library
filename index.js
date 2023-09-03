8069d1acc36844f5f253a32175231667b41fa57a
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-09-03 16:01:25 +0500

    "setup tailwind"

---
+++ index.js
@@ -0,0 +1,10 @@
+let library = [
+  {
+    title: "Elone Musk",
+    author: "Waler Isecson",
+    Genre: "Non fiction",
+    Pages: 355,
+    cover: "./imgs/musk.jpg",
+    status: "Read",
+  },
+];
