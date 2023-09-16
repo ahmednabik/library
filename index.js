1d0782f7a35e07943297df9c45f1fa0f88c755e7
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-09-16 22:57:51 +0500

    New working version with improved UI

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const pages = document.getElementById("pages");
 const status = document.getElementById("status");
 const description = document.getElementById("description");
 const formElement = document.querySelector("form");
-const bookCard = document.getElementById("card-section");
+const bookCard = document.getElementById("book-shelf");
 const clearBtn = document.getElementById("clear-library");
+const addBookModal = document.getElementById("add-book-modal");
+const addBookBtn = document.getElementById("add-book");
+const closeModalBtn = document.getElementById("cancel-button");
+
+addBookBtn.addEventListener("click", () => {
+  addBookModal.showModal();
+});
+
+closeModalBtn.addEventListener("click", () => {
+  addBookModal.close();
+});
 
 storedItems = localStorage.getItem("lib");
+
 if (storedItems) {
   library = JSON.parse(storedItems);
   librarySize = library.length;
@@ -17,10 +29,10 @@ if (storedItems) {
 } else {
   library = [];
   librarySize = 0;
-  document.getElementById("card-section").innerHTML =
-    "<div>No books found.</div>";
+  document.getElementById(
+    "book-shelf"
+  ).innerHTML = `<div style="padding-top:50px">No books found.</div>`;
 }
-
 formElement.addEventListener("submit", addBook); //add book
 clearBtn.addEventListener("click", clearLibrary); //delete library
 
@@ -28,23 +40,26 @@ function displayBooks() {
   bookCard.innerHTML = "";
   bookCard.innerHTML = library
     .map((book) => {
-      return `<div class="max-w-2xl mx-auto">
-    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-        <div class="cover grid place-items-center"><img id="cover" class="transition-all duration-300 ease-in-out hover:scale-95 object-cover object-center w-80 h-80 rounded-t-lg" src="${book.cover}" alt=""></div>
-        <div class="p-5">
-            <h5 id="title" class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">${book.title}</h5>
-            <p id="description" class="font-normal text-gray-700 mb-3 dark:text-gray-400">${book.description}</p>
-        </div>
-        <div class="flex justify-around items-center  gap-4 text-xs mb-4">
-            <div><span><strong>Author</strong> </span><p id="author">${book.author}</p></div>
-            <div><span><strong>Pages</strong> </span><p id="pages">${book.pages}</p></div>
-            <div><span><strong>Status</strong> </span><p id="status">${book.status}</p></div>
-            <div><span><strong>Genre</strong> </span><p id="genre">${book.genre}</p></div>
-            <div style="display:none"><span><strong>Genre</strong> </span><p id="index">${book.index}</p></div>
-        </div>
-        <div> <button onClick="removeBook(event)" data-index=${book.index} class="delete-book">X</button></div>
-    </div>
-</div>`;
+      return `
+            <div class="card">
+                <div class="cover">
+                    <div style="background-image: url(${book.cover});" class="cover-image"></div>
+                </div>
+                <div class="info">
+                    <p class="title">${book.title}</p>
+                    <p class="author">By: ${book.author}</p>
+                    <div class="description">
+                        <p>
+                        ${book.description}
+                        </p>
+                    </div>
+                    <p class="genre"><strong>Genre:</strong> ${book.genre}</p>
+                    <p class="page-numbers"><strong>Pages:</strong> ${book.pages}</p>
+                    <button class="btn remove">Remove</button>
+                    <button class="btn mark">Mark Read</button>
+                </div>
+            </div>
+      `;
     })
     .join("");
 }
